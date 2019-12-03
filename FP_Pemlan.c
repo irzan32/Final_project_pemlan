4d8bbd8cfae6a3cd06ac97cfd1a199747656e4ba
irzan32 <irsyadzan003@gmail.com>
2019-12-03 13:32:15 +0700

    first commit

---
+++ FP_Pemlan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	printf("Masukkan nama anda :");
+	printf("apa keinginan anda?");
+	printf("1. Membuat data word");
+	printf("2. Membuat data excel");
+	gets("1")
+	return 0;
+}
